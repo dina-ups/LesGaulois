7309c9842b370c73db393df8cb721496f77c5199
tamboura dina <tmd4384a@ens.fsi>
2022-09-26 20:09:15 +0200

    depot_modifications

---
+++ src/histoire/scenario.java
@@ -0,0 +1,22 @@
+package histoire;
+
+import personnages.Gaulois;
+import personnages.Romain;
+
+public class scenario {
+	public static void main(String[] args) {
+		Gaulois asterix= new Gaulois("asterix",8 );
+		System.out.println(asterix.prendreParole());
+		asterix.parler("bonjour à tous");
+		Romain Minus= new Romain("Minus",6 );
+		Minus.prendreParole();
+		Minus.parler("UN GAU...UN GAUGAU");
+		asterix.frapper(Minus);
+
+		asterix.frapper(Minus);
+	
+		asterix.frapper(Minus);
+		
+
+}
+}
